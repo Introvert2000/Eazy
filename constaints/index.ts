089e1be43a75638eb748528e92ea1938ced3cbd1
Introvert2000 <alvareschamp200@gmail.com>
2024-02-27 01:02:03 +0530

    added stuff

---
+++ constaints/index.ts
@@ -0,0 +1,86 @@
+// NAVIGATION
+export const NAV_LINKS = [
+    { href: '/', key: 'home', label: 'Home' },
+    { href: '/', key: 'Book_a_Table', label: 'Book a Table' },
+    { href: '/', key: 'Order_Online', label: 'Deliveries' },
+    { href: '/', key: 'pricing ', label: 'Pricing ' },
+    { href: '/', key: 'contact_us', label: 'Contact Us' },
+  ];
+  
+  // CAMP SECTION
+  export const PEOPLE_URL = [
+    '/person-1.png',
+    '/person-2.png',
+    '/person-3.png',
+    '/person-4.png',
+  ];
+  
+  // FEATURES SECTION
+  export const FEATURES = [
+    {
+      title: 'Try new Cuisines everyday',
+      icon: '/map.svg',
+      variant: 'green',
+      description:
+        'We provide new the details of various restaurant near to your homes',
+    },
+    {
+      title: 'Set an adventure for your palette',
+      icon: '/calendar.svg',
+      variant: 'green',
+      description:
+        "Order and enjoy various cuisines in the comforts of your home",
+    },
+    {
+      title: 'Choices found by our Revolutionary AI model',
+      icon: '/tech.svg',
+      variant: 'green',
+      description:
+        'Technology uses augmented reality as a guide to your cuisine journey . Already supported by the latest technology without an internet connection',
+    },
+    {
+      title: 'Many new locations every month',
+      icon: '/location.svg',
+      variant: 'orange',
+      description:
+        'Lots of new locations every month, because we have a worldwide community of climbers who share their best experiences with climbing',
+    },
+  ];
+  
+  // FOOTER SECTION
+  export const FOOTER_LINKS = [
+    {
+      title: 'Learn More',
+      links: [
+        'About Eazy',
+        'Press Releases',
+        'Environment',
+        'Jobs',
+        'Privacy Policy',
+        'Contact Us',
+      ],
+    },
+    {
+      title: 'Our Community',
+      links: ['Blinkit xixixi', 'Eazy Diner', 'Happy Dinner'],
+    },
+  ];
+  
+  export const FOOTER_CONTACT_INFO = {
+    title: 'Contact Us',
+    links: [
+      { label: 'Admin Officer', value: '123-456-7890' },
+      { label: 'Email Officer', value: 'Eazy@akinthil.com' },
+    ],
+  };
+  
+  export const SOCIALS = {
+    title: 'Social',
+    links: [
+      '/facebook.svg',
+      '/instagram.svg',
+      '/twitter.svg',
+      '/youtube.svg',
+      '/wordpress.svg',
+    ],
+  };
